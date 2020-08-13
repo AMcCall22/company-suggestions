4ecb7766df79f933daa846e0fbd834b6b4422da5
AMcCall22 <alisonlmccallum@gmail.com>
2020-08-13 11:35:14 +0000

    company_list functionality added

--- app.py
+++ app.py
@@ -16,17 +16,43 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+
 # Displays all companies on home page
 @app.route("/")
 @app.route("/show_company")
 def show_company():
     company = mongo.db.companies.find()
     return render_template("companies.html", show_company=company)
+    return render_template("all_companies.list.html", show_company=company)
+
+
+@app.route("/list_company")
+def list_company():
+    company = mongo.db.companies.find()
+    return render_template("all_companies_list.html", list_company=company)
 
 
 # Connects to company_type collection in MongoDB for dropdown options
 @app.route("/add_company", methods=["GET", "POST"])
 def add_company():
+    if request.method == "POST":
+        company = {
+            "company_type": request.form.get("company_type"),
+            "company_name": request.form.get("company_name"),
+            "sector": request.form.get("sector"),
+            "description": request.form.get("description"),
+            "url": request.form.get("url"),
+            "remote": request.form.get("remote"),
+            "level_of_positions": request.form.get("level_of_positions"),
+        }
+        mongo.db.companies.insert_one(company)
+    company_type = mongo.db.company_type.find()
+    return render_template("add_company.html", company_type=company_type)
+
+
+# Search all companies and provide view 
+@app.route("/search_company", methods=["GET"])
+def search_company():
     if request.method == "POST":
         company = {
             "company_type": request.form.get("company_type"),
